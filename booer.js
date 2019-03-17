afae287c048305a87061b727305920cdb12ae70a
William Pollock <wmpollock@gmail.com>
2019-03-17 01:43:44 -0700

    Initial commit

---
+++ booer.js
@@ -0,0 +1,121 @@
+
+var ambientTimer, ambientCountdownTimer;
+
+function playRandomBoo() {
+  var $boos = $("audio.boos"),
+  i = Math.round(Math.random() * ($boos.length - 1)),
+  $boo = $boos[i];
+  message("Last played: " + $($boo).attr('src'))
+  $boo.play();
+  console.log(i);
+//  console.log($boo);
+}
+
+$("document").ready(function() {
+  $(".instaboo button").click(playRandomBoo);
+  $(".ambientboo button.start").click(startAmbientBoo);
+  $(".ambientboo .info button.cancel").click(cancelAmbient);
+
+  // http://seiyria.com/bootstrap-slider/
+  // Refernce: https://github.com/seiyria/bootstrap-slider
+
+  $slider = $(".ambientboo input.slider").slider({
+    // Tooltip apparently clashes with 4.0 ? :|
+    tooltip: 'never',
+    //    tooltip_split: true, 
+    min: 10,
+    max: 300,
+    range: true, // FER YEAH, BUT WHERE'S MY KLASSES?!?
+    value: [30, 80],
+    step: 10,
+  }).on("change", function() { 
+    var vals = $(".ambientboo input.slider").val().split(',');
+    $(".ambientboo label.min").text(fmtCountdown(vals[0]));
+    $(".ambientboo label.max").text(fmtCountdown(vals[1]));
+
+    //$(".ambientboo label.min").text(vals[0] + " seconds");
+//    $(".ambientboo label.max").text(vals[1] + " seconds");
+
+
+    console.log("hello! Don't go changing!")}).change();
+
+
+});
+function startAmbientBoo() {
+  console.log("AMBIENT BOO.  YIKLES!!");
+  randomAmbientWait();
+}
+
+function randomAmbientWait() {
+  var vals = $(".ambientboo input.slider").val().split(','),
+  range = vals[1] - vals[0],
+  nextInterval = parseInt(vals[0]) + Math.round(Math.random() * range),
+  now = new Date(),
+  then = new Date(nextInterval * 1000 + now.getTime());;
+//  if(play) {
+    playRandomBoo();
+//  }
+
+  ambientMessage("Next boo will execute at " + then.toTimeString() + ".");
+  ambientCountDown(nextInterval);
+  ambientTimer = setTimeout(function() {randomAmbientWait(1)}, nextInterval * 1000);
+}
+
+function ambientCountDown(interval) {
+  $(".ambientboo .info p.seconds-left").text("Booing in " + fmtCountdown(interval) + ".");
+  ambientCountdownTimer = setTimeout(function() {
+    $(".ambientboo .info p.seconds-left").text(--interval +  " seconds left");
+    if (interval > 0) {
+      ambientCountDown(interval); 
+    }
+  },
+				     1000
+				    );
+}
+
+function fmtCountdown(interval) {
+  var minutes = Math.floor(interval / 60),
+  seconds = interval - (minutes * 60), 
+  timeStr = '';
+
+  
+  if (minutes > 1) {
+    timeStr = minutes + " minutes, "
+  } else if (minutes === 1) {
+    timeStr = "1 minute, "
+  }
+  if (seconds > 1) {
+    timeStr = timeStr + seconds + " seconds";
+  } else if (seconds === 1) {
+    timeStr = timeStr + "1 second";
+  } else {
+    timeStr = timeStr + "exactly";
+  }
+ 
+  return timeStr;
+}
+
+function ambientMessage(msg) {
+  $(".ambientboo .info p.wait-till").text(msg);
+  $(".ambientboo .info").fadeIn();
+  $(".ambientboo .info :hidden").fadeIn();
+  $(".ambientboo .btn-primary").fadeOut(); // Hide activate button again
+}
+
+function message(msg) {
+  $(".card.status p").text(msg).closest('.card').show();
+}
+
+function clearTimers() {
+  clearTimeout(ambientCountdownTimer);
+  clearTimeout(ambientTimer);
+
+}
+
+
+function cancelAmbient() {
+  ambientMessage("Ambient playback stopped.");
+  $(".ambientboo .info .on-when-active").fadeOut();
+  $(".ambientboo .btn-primary").fadeIn(); // Show activate button again
+  clearTimers();
+}
---
+++ index.php
@@ -0,0 +1,96 @@
+<?php
+
+
+
+// PageGen instantiation
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/Phplib/PageGen/Bootstrap4/Superhero.php");
+
+$page = new Superhero(array(
+    title => 'Random Boo-er',
+    favicon => file_get_contents('favicon/favicon.include'),
+    css => array(
+        'booer.css'
+    ), // Should do filectime internally
+    js => array(
+        'booer.js'
+    ),
+    additional_head => '
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.9.0/css/bootstrap-slider.min.css" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.9.0/bootstrap-slider.min.js"></script>
+',
+    additional_footer => '
+  <!-- Slider control -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.9.0/bootstrap-slider.min.js"></script>
+'
+));
+
+// At this point, anything that comes out is wrapped as template content :)
+?>
+
+<div class="container">
+  <div class="jumbotron">
+    <h1>Random Boo-er</h1>
+    <p>Provides ambient booing for ongoing ego adjustment as well asl realtime booing for realtime environmental enhancement.</p>
+    <p>Suitable for running in the background during conference calls.</p>
+  </div>
+  
+  
+  <?php
+$sounddir = "sounds/";
+$count = 0;
+// while ($file = preg_grep('~\.(mp3|wav)$~', scandir($sounddir))) {
+foreach (preg_grep('~\.(mp3|wav)$~', scandir($sounddir)) as $file) {
+    // echo("<p>$file</p>");
+    echo ("<audio class='boos' src='$sounddir$file' preload='auto'></audio>\n");
+}
+?>
+  
+  <div class="card text-white bg-success status hidden">
+    <div class="card-body">
+      <p>Please start with the functions below.</p>
+    </div>
+  </div>
+
+
+  <div class="card text-white bg-primary ambientboo">
+    <div class="card-body">
+      <h2>Ambient Booing</h2>
+
+      <p>Ambient background booing.</p>
+
+      <div class="card text-white bg-info info hidden">
+        <div class="card-body">
+          <p class="wait-till">i last played nothing</p>
+          <p class="seconds-left on-when-active">n seconds left</p>
+          <button type="button" class="btn btn-warning cancel on-when-active">Cancel</button>
+        </div>
+      </div>
+
+
+      <label>Delay range:</label>
+      <div>
+        <input type="text" class="slider" value="" />
+        <p class="smallLabel">
+          Somewhere between
+          <label class="min">0 secs</label>
+          and
+          <label class="max">5 secs</label>
+          .
+        </p>
+      </div>
+
+      <button type="button" class="btn btn-primary start">Boo! &#x1F345;</button>
+    </div>
+  </div>
+
+
+  <div class="card text-white bg-primary instaboo">
+    <div class="card-body">
+      <h2>Insta-booer</h2>
+      <p>Instant random booing.</p>
+      <button type="button" class="btn btn-primary">Boo! &#x1F345;</button>
+    </div>
+  </div>
+</div>
+
+
